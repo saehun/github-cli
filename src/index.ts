16acca926a851557d0d072881c06c69400a9d927
Karl Saehun Chung <nycom13@gmail.com>
2019-11-06 22:35:58 +0900

    save

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,123 @@
-import * as execa from "execa";
-import * as program from "commander";
+import execa from "execa";
+import program from "commander";
 import ora from "ora";
+import Octokit from "@octokit/rest";
+import fetch from "node-fetch";
 const chalk = require("chalk");
 const git = require("simple-git")();
 const version = require("../package.json").version;
 
+const repo = { origin: "", upstream: "", name: "" };
+
+const getRepo = async () => {
+  return new Promise((resolve) => {
+    if (repo.name) {
+      resolve(repo);
+    } else {
+      const regex = /^git@github.com:(.*)\/(.*).git$/;
+      git.getRemotes(true, (err: any, result: any) => {
+        if (err) process.exit(5);
+        const originParsed = regex.exec(result.find((x: any) => x.name === "origin")?.refs.push);
+        if (!originParsed) {
+          console.log("Origin is not set");
+          process.exit(1);
+        }
+        repo.origin = originParsed[1];
+        repo.name = originParsed[2];
+        const upstreamParsed = regex.exec(result.find((x: any) => x.name === "upstream")?.refs.push);
+        if (!upstreamParsed) {
+          console.log("Upstream is not set");
+          process.exit(1);
+        }
+        repo.upstream = upstreamParsed[1];
+        resolve(repo);
+      });
+    }
+  });
+};
+
+const initializeGit = async () => {
+  await getRepo();
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_ACCESS_TOKEN,
+    userAgent: `github-cli v${version}`,
+    previews: ["jean-grey", "symmetra"],
+    timeZone: "Asia/Seoul",
+    baseUrl: "https://api.github.com",
+    log: {
+      debug: () => { },
+      info: () => { },
+      warn: console.warn,
+      error: console.error
+    },
+  });
+
+  return octokit;
+};
+
 const getCurrentBranchName = async () => {
-  return new Promise<string>((resolve, reject) => {
+  const spinner = ora("get current branch name...").start();
+  return new Promise<string>((resolve) => {
     git.branch([], (err: any, result: { current: string }) => {
-      if (err) reject(err); resolve(result.current);
+      if (err) {
+        spinner.fail();
+        process.exit(1);
+      } else {
+        spinner.text = "get current branch name... " + chalk.yellow(result.current);
+        spinner.succeed();
+        resolve(result.current);
+      }
     });
   });
 };
 
 const pushOrigin = async (branchName: string) => {
-  const spinner = ora("git push origin" + chalk.cyan(branchName)).start();
-  return new Promise((resolve, reject) => {
+  const spinner = ora("git push origin " + chalk.yellow(branchName)).start();
+  return new Promise((resolve) => {
     git.push(["origin", branchName], (err: any, result: any) => {
-      console.log(result);
-      spinner.stop();
-      if (err) reject(err); resolve(result);
+      if (err) {
+        spinner.fail();
+        process.exit(2);
+      } else {
+        spinner.succeed();
+        resolve(result);
+      }
     });
   });
 };
 
+const getJiraIssue = async (key: string) => {
+  const spinner = ora("get jira issue of id " + chalk.yellow(key)).start();
+  const result = await execa("jira", ["show", "-s", key]);
+  if (result.failed || result.exitCode !== 0) {
+    spinner.fail();
+    process.exit(3);
+  } else {
+    spinner.succeed();
+    console.log("└─", result.stdout);
+    return result.stdout;
+  }
+};
+
+const makePullRequest = async () => {
+  const octokit = await initializeGit();
+  const res = await octokit.pulls.create({
+    owner: repo.upstream,
+    repo: repo.name,
+    title: "hmmmmmmmmmmmmmm",
+    head: "minidonut:3823",
+    base: "master"
+  });
+  console.log(res);
+};
+
+
 const list = async (...args: any) => {
-  const current = await getCurrentBranchName();
-  console.log(current);
+  /* const current = await getCurrentBranchName(); */
+  /* await getJiraIssue(current); */
+  /* console.log(await getRepo()); */
+  /* initializeGit(); */
+  makePullRequest();
 };
 
 const push = async () => {
