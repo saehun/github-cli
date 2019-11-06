5f95ab6643704210317b6098d086699e75285c03
Karl Saehun Chung <nycom13@gmail.com>
2019-11-06 21:08:18 +0900

    Add getCurrentBranchName function

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,24 @@ import * as program from "commander";
 const git = require("simple-git")();
 const version = require("../package.json").version;
 
-const list = async (...args: any) => {
-  const branch = git.branch([], (...args: any) => {
-    console.log(args);
+const getCurrentBranchName = async () => {
+  return new Promise((resolve, reject) => {
+    const branch = git.branch([], (...args: any) => {
+      const current = args[1]?.current;
+      if (current) {
+        resolve(current);
+      } else {
+        reject("Unexpected Error");
+      }
+    });
   });
+};
+
+
+
+const list = async (...args: any) => {
+  const current = await getCurrentBranchName();
+  console.log(current);
 };
 
 /**
